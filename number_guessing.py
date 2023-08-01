da078cc8528cb0e4e7ab843d22e4caf0bcb66fa5
Xcoder <chindaebube03@gmail.comm>
2023-08-01 14:42:46 +0100

    refractored the code to make use of classes

--- number_guessing.py
+++ number_guessing.py
@@ -1,40 +1,60 @@
 import random
 
-def number_guessing_game():
-    number = random.randint(0, 999)
-    attempts = 0
-    max_attempts = 5
-    
-    print("**************************************************")
-    print("                                                  ")
-    print("        Welcome to the Number Guessing Game!")
-    print("                                                  ")
-    print("**************************************************\n")
-    print("I have chosen a number between 0 and 999. You have", max_attempts, "attempts to guess it.\n")
+class NumberGuessingGame:
+    def __init__(self):
+        self.secret_number = random.randint(0, 999)
+        self.max_attempts = 5
+        self.attempts = 0
 
-    while attempts < max_attempts:
-        print("--------------------------------------------------")
-        print("Attempt:", attempts+1)
-        
+    def check_guess(self, guess):
+        self.attempts += 1
+        if guess == self.secret_number:
+            return "Congratulations! You guessed the correct number in {} attempts!\n".format(self.attempts)
+        elif guess < self.secret_number:
+            return "Wrong guess! The number is greater than {}\n".format(guess)
+        else:
+            return "Wrong guess! The number is smaller than {}\n".format(guess)
+
+class GameUI:
+    def __init__(self):
+        print("**************************************************")
+        print("                                                  ")
+        print("        Welcome to the Number Guessing Game!")
+        print("                                                  ")
+        print("**************************************************\n")
+
+    def get_guess(self):
         try:
-            guess = int(input("Enter your guess: "))
+            return int(input("Enter your guess: "))
         except ValueError:
             print("\033[91mInvalid input! Please enter a valid number.\033[0m")
-            continue
+            return None
 
-        attempts += 1
+    def display_message(self, message):
+        print(message)
 
-        if guess == number:
-            print("Congratulations! You guessed the correct number in", attempts, "attempts!\n")
-            return
-        elif guess < number:
-            print("Wrong guess! The number is greater than", guess)
-        else:
-            print("Wrong guess! The number is smaller than", guess)
+    def display_game_over(self, secret_number):
+        print("\n**************************************************")
+        print("                Game Over!")
+        print("**************************************************\n")
+        print("You ran out of attempts. The number was:", secret_number)
 
-    print("\n**************************************************")
-    print("                Game Over!")
-    print("**************************************************\n")
-    print("You ran out of attempts. The number was:", number)
+def main():
+    ui = GameUI()
+    game = NumberGuessingGame()
 
-number_guessing_game()
+    while game.attempts < game.max_attempts:
+        ui.display_message("Attempt: {}".format(game.attempts + 1))
+        guess = ui.get_guess()
+
+        if guess is not None:
+            result = game.check_guess(guess)
+            ui.display_message(result)
+
+            if "Congratulations!" in result:
+                return
+
+    ui.display_game_over(game.secret_number)
+
+if __name__ == "__main__":
+    main()
